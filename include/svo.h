c04353c54cd765bfd00383520f95faaa0438d57a
Ludw <ludwig-geyer@web.de>
2024-07-10 23:33:04 +0200

    backup

--- include/svo.h
+++ include/svo.h
@@ -15,11 +15,6 @@
 #define MAX_DEPTH 8
 #define CHILD_COUNT 8
 
-// 15-bit integer limit = 32 * 32 * 32
-#define NODES_PER_PAGE 32768
-#define DEFAULT_FP_SPACE 1024 // default far pointer count
-#define PAGE_SIZE 32768
-
 #define DEFAULT_MAT 1
 
 #define SET_BIT(num, bit) ((num) | (1 << (bit)))
@@ -27,7 +22,7 @@
 #define CHECK_BIT(num, bit) (((num) & (1 << (bit))) != 0)
 
 struct SvoNode {
-    uint16_t children_index;
+    uint32_t children_index;
     uint8_t child_mask;
     uint8_t mat;
 
@@ -61,88 +56,32 @@ struct SvoNode {
     bool exists_child(const uint8_t index) {
         return CHECK_BIT(child_mask, index);
     }
-
-    void set_children_index(const uint16_t index, const bool far = false) {
-        children_index = far ? SET_BIT(index, 15) : RESET_BIT(index, 15);
-    }
-
-    void read_children_index(uint16_t &index, bool &far) {
-        index = children_index & 0x7FFF;
-        far = children_index & 0x8000;
-    }
-};
-
-struct FarPointer {
-    uint16_t page_index;
-    uint16_t children_index;
-};
-
-struct SvoPage {
-    std::vector<SvoNode> nodes;
-    std::vector<FarPointer> far_pointers;
-
-    SvoPage() {
-        nodes.reserve(NODES_PER_PAGE);
-        far_pointers.reserve(DEFAULT_FP_SPACE);
-    }
-
-    uint16_t available_nodes() const {
-        return NODES_PER_PAGE - nodes.size();
-    }
 };
 
 class Svo {
 public:
-    std::vector<SvoPage> pages;
+    std::vector<SvoNode> nodes;
 
     Svo(const std::vector<uint8_t> &vox_grid, const uint32_t chunk_res) {
-        pages.push_back(SvoPage());
-        pages[0].nodes.push_back(SvoNode()); // add root
+        nodes.push_back(SvoNode()); // add root
 
         for (size_t morton_index = 0; morton_index < vox_grid.size(); morton_index++) {
             const uint8_t mat = vox_grid[morton_index];
 
             if (mat > 0) {
-                uint16_t page_index = 0;
-                SvoPage &current_page = pages[page_index]; // page with root
-                SvoNode &current = current_page.nodes[0]; // root
-
+                SvoNode &current = nodes[0]; // start at root
                 uint32_t res = chunk_res;
 
                 for (uint8_t depth = 0; depth < MAX_DEPTH; depth++) {
-                    const uint32_t child_size = (res * res * res) / CHILD_COUNT; // current_size / child_count
+                    // current_size / child_count = child_size
+                    const uint32_t child_size = (res * res * res) / CHILD_COUNT;
+                    // determine in which child the morton index is
                     const uint32_t child_idx = static_cast<uint32_t>(morton_index) / child_size;
 
-                    if (current.child_mask == 0) {
-                        if (current_page.available_nodes() < CHILD_COUNT) {
-                            SvoPage &far_page = current_page;
-                            uint16_t far_page_index = page_index;
-
-                            while (far_page.available_nodes() < CHILD_COUNT) {
-                                far_page_index++;
-
-                                if (far_page_index == pages.size()) {
-                                    pages.push_back(SvoPage());
-                                    break;
-                                }
-
-                                far_page = pages[far_page_index];
-                            }
-
-                            current.set_children_index(current_page.far_pointers.size(), true);
-
-                            FarPointer far_pointer;
-                            far_pointer.page_index = far_page_index;
-                            far_pointer.children_index = static_cast<uint16_t>(far_page.nodes.size());
-                            current_page.far_pointers.push_back(far_pointer);
-
-                            for (int i = 0; i < CHILD_COUNT; i++) {
-                                far_page.nodes.push_back(SvoNode());
-                            }
-                        } else {
-                            for (int i = 0; i < CHILD_COUNT; i++) {
-                                current_page.nodes.push_back(SvoNode());
-                            }
+                    // if leaf subdivide
+                    if (current.is_leaf()) {
+                        for (int i = 0; i < CHILD_COUNT; i++) {
+                            current_page.nodes.push_back(SvoNode());
                         }
                     }
 
